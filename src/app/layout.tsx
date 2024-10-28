d1dd3c722e04a786f5372d0fc6eeafd64aabb959
peterlidee <peter@lidee.be>
2024-10-28 11:59:19 +0100

    Updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'searchParams in Next 15',
@@ -12,7 +13,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        <div className='p-2 flex flex-col items-center gap-6 text-lg'>
+          <nav className='flex gap-3'>
+            <Link href='/' className='text-blue-700 underline'>
+              home
+            </Link>
+            <Link href='/asyncpage?foo=bar' className='text-blue-700 underline'>
+              async page
+            </Link>
+            <Link href='/syncpage?foo=bar' className='text-blue-700 underline'>
+              sync page
+            </Link>
+            <Link href='/list' className='text-blue-700 underline'>
+              list
+            </Link>
+          </nav>
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
