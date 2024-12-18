31f2a65dd344c48ac8695a6788e7a861eb425c02
peterlidee <peter@lidee.be>
2024-12-18 17:30:59 +0100

    Updated Home test

--- src/app/__tests__/page.test.tsx
+++ src/app/__tests__/page.test.tsx
@@ -3,8 +3,9 @@ import { screen, render } from '@testing-library/react';
 import Home from '@/app/page';
 
 describe('<Home />', () => {
-  test('It renders', () => {
-    render(<Home />);
+  test('It renders', async () => {
+    const component = await Home();
+    render(component);
     expect(screen.getByText(/hello world/i)).toBeInTheDocument();
   });
 });
