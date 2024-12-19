6b8a5c1b707c487c775a76b643379ef029b31103
peterlidee <peter@lidee.be>
2024-12-19 11:17:26 +0100

    updated valideSortOrderT test

--- src/lib/__tests__/validateSortOrder.test.ts
+++ src/lib/__tests__/validateSortOrder.test.ts
@@ -1,25 +1,48 @@
 import validateSortOrder from '../validateSortOrder';
+import isSortOrderT from '@/types/isSortOrderT';
+jest.mock('@/types/isSortOrderT');
 
 describe('function validateSortOrder', () => {
   test('It returns "asc" when value is undefined', () => {
-    expect(validateSortOrder(undefined)).toBe('asc');
+    (isSortOrderT as unknown as jest.Mock).mockReturnValue('asc');
+    const result = validateSortOrder(undefined);
+    expect(isSortOrderT).not.toHaveBeenCalled();
+    expect(result).toBe('asc');
   });
   test('It returns "asc" when value is "null"', () => {
-    expect(validateSortOrder(null)).toBe('asc');
+    (isSortOrderT as unknown as jest.Mock).mockReturnValue('asc');
+    const result = validateSortOrder(null);
+    expect(isSortOrderT).not.toHaveBeenCalled();
+    expect(result).toBe('asc');
   });
   test('It returns "asc" when value is ""', () => {
-    expect(validateSortOrder('')).toBe('asc');
+    (isSortOrderT as unknown as jest.Mock).mockReturnValue('asc');
+    const result = validateSortOrder('');
+    expect(isSortOrderT).not.toHaveBeenCalled();
+    expect(result).toBe('asc');
   });
   test('It returns "asc" when value is empty array', () => {
-    expect(validateSortOrder([])).toBe('asc');
+    (isSortOrderT as unknown as jest.Mock).mockReturnValue('asc');
+    const result = validateSortOrder([]);
+    expect(isSortOrderT).not.toHaveBeenCalled();
+    expect(result).toBe('asc');
   });
   test('It returns "asc" when value is array', () => {
-    expect(validateSortOrder(['a', 'b', 'c'])).toBe('asc');
+    (isSortOrderT as unknown as jest.Mock).mockReturnValue('asc');
+    const result = validateSortOrder(['a', 'b', 'c']);
+    expect(isSortOrderT).not.toHaveBeenCalled();
+    expect(result).toBe('asc');
   });
   test('It returns "asc" when value is "asc"', () => {
-    expect(validateSortOrder('asc')).toBe('asc');
+    (isSortOrderT as unknown as jest.Mock).mockReturnValue('asc');
+    const result = validateSortOrder('asc');
+    expect(isSortOrderT).toHaveBeenCalledWith('asc');
+    expect(result).toBe('asc');
   });
   test('It returns "desc" when value is "desc"', () => {
-    expect(validateSortOrder('desc')).toBe('desc');
+    (isSortOrderT as unknown as jest.Mock).mockReturnValue('desc');
+    const result = validateSortOrder('desc');
+    expect(isSortOrderT).toHaveBeenCalledWith('desc');
+    expect(result).toBe('desc');
   });
 });
